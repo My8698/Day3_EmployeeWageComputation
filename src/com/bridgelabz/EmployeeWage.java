338e3feb848055f724c23f46914afc71b7684157
devraajmadhuri@gmail.com <devraajmadhuri@gmail.com>
2022-09-30 18:13:52 +0530

    Added:Welcome to Employee Wage problem

---
+++ src/com/bridgelabz/EmployeeWage.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class EmployeeWage {
+    public static void main (String[] args){
+        System.out.println("Welcome to Employee Wage Problem");
+    }
+
+
+
+
+
+}
